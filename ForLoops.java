6dbc6a2576fd81d36b6a8159d81b068ce7380a90
Anukool <anukoolsharma140@gmail.com>
2023-06-24 16:50:25 +0530

    First PAttern Problem

--- ForLoops.java
+++ ForLoops.java
@@ -3,7 +3,11 @@ public class ForLoops {
     {
         for(int i=0;i<=5;i++)
         {
-            System.out.println("*");
+            for(int j =0;j<=i;j++)
+            {
+            System.out.print("*");
+            }
+            System.out.println( );
         }
     }    
 }
