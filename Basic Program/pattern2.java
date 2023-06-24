cb100c97fb6ce3313038776eafa7a8125abd0752
Anukool <anukoolsharma140@gmail.com>
2023-06-24 20:12:43 +0530

    Pattern 2

--- Basic Program/pattern2.java
+++ Basic Program/pattern2.java
@@ -1,3 +1,24 @@
 public class pattern2 {
+    public static void main(String[] args)
+    {
+        int c=1;
+        for(int i=1;i<6;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+                if(j%2!=0)
+                {
+                    System.out.print("  "+c+"  ");
+                    ++c;
+                }
+                else
+                {
+                    System.out.print("  "+c+"  ");
+                    --c;
+                }
+            }
+            System.out.println();
+        }
+    }
     
 }
