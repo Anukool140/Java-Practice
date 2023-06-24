3c91abe03500ef0ee0cf9a2a4662eb3b30a26b6d
Anukool <anukoolsharma140@gmail.com>
2023-06-24 18:38:35 +0530

    rearrange order

--- Basic Program/Sumoftwonumbers.java
+++ Basic Program/Sumoftwonumbers.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers.");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("Enter two numbers.");
+        
         int sum = a + b;
         System.out.println("SUM = "+sum);
     }
